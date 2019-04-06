11785ec068a8fff5995cb7f89e8a421865e64793
Declan Smyth <declans@avaya.com>
2019-04-07 00:34:03 +0100

    Add notification module

---
+++ scripts/notification.py
@@ -0,0 +1,18 @@
+# #################################### #
+# Author: Declan Smyth
+# Email : declan.smyth@gmail.com
+# #################################### #
+# 
+# This script will send notification infomration
+# to AWS SNS and CloudWatch 
+# available in ECS
+# ######################################
+import boto3
+
+#-------------------------------------------
+# Function Definitions
+#-------------------------------------------
+
+
+
+#-------------------------------------------

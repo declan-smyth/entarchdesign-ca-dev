aee885bbc7a496fc8eedb42b0d6b7894f3ad392b
Declan Smyth <declans@avaya.com>
2019-04-07 21:40:43 +0100

    Implement Email SNS Notification

--- scripts/notification.py
+++ scripts/notification.py
@@ -8,11 +8,22 @@
 # available in ECS
 # ######################################
 import boto3
+import json
+
+client = boto3.client('sns')
+ARN='arn:aws:sns:eu-west-1:197110341471:AWS-Test-Results'
 
 #-------------------------------------------
 # Function Definitions
 #-------------------------------------------
-
+def SendEmailNotification(message):
+    response = client.publish(
+        TargetArn=ARN,
+        Message=json.dumps(
+            {'default': json.dumps(message)}),
+        Subject='A short message',
+        MessageStructure='json'
+    )
 
 
 #-------------------------------------------

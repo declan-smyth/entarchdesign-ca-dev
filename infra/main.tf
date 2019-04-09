f50a5b515fd9a3a4fec072251cda07b646933cfc
Declan Smyth <declans@avaya.com>
2019-04-09 20:54:59 +0100

    Added bug fixes and resources to fully deploy the environment

--- infra/main.tf
+++ infra/main.tf
@@ -20,10 +20,11 @@ resource "aws_lb" "EAD-CA-LOADBALANCER" {
   internal            = false
   load_balancer_type  = "application"
   subnets             = ["${data.aws_subnet_ids.all.ids}"]
-  security_groups     = ["${aws_security_group.EAD-CA-SECURITYGROUP.id}"]
-  
+  security_groups     = ["${aws_security_group.EAD-CA-SECURITYGROUP-LOADBALANCER.id}"]
+  idle_timeout        = "60"
 
   tags = {
+    Name              = "${var.KEY_NAME}-loadbalancer"
     Author            = "${var.CA_AUTHOR_NAME}"
     Purpose           = "${var.CA_PURPOSE}"
     Environment       = "${var.CA_ENVIRONMENT}"
@@ -32,22 +33,21 @@ resource "aws_lb" "EAD-CA-LOADBALANCER" {
 }
 
 resource "aws_lb_target_group" "EAD-CA-TARGETGRP" {
-  name                = "${var.KEY_NAME}-targettroup"
-  port                = 80
-  protocol            = "HTTP"
-  
-  
+  name                = "${var.KEY_NAME}-targetgroup"
+  port                = "${var.PORT_HTTP}"
+  protocol            = "${var.PROTOCOL_HTTP}"
+   
   vpc_id              = "${data.aws_vpc.default.id}"
   target_type         = "instance"
-
+  
   
   health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    interval            = 30
-    protocol            = "HTTP"
-    matcher             = "200"
+    healthy_threshold   = 5 # Number of consecutive passing checks for Healthy 
+    unhealthy_threshold = 2 # Number of consecutive failing checks for UnHealthy
+    timeout             = 5 # Time in Seconds that no-response equals UnHealthy
+    interval            = 30 # Time Between Health Checks on an instance
+    protocol            = "${var.PROTOCOL_HTTP}"
+    matcher             = "200" # Codes to indicate a successful responce
   }
 
   tags = {
@@ -59,8 +59,8 @@ resource "aws_lb_target_group" "EAD-CA-TARGETGRP" {
 
 resource "aws_lb_listener" "EAD-CA-LB-LISTENER" {
   load_balancer_arn   = "${aws_lb.EAD-CA-LOADBALANCER.arn}"
-  port                = "80"
-  protocol            = "HTTP"
+  port                = "${var.PORT_HTTP}"
+  protocol            = "${var.PROTOCOL_HTTP}"
 
   default_action {
     target_group_arn  = "${aws_lb_target_group.EAD-CA-TARGETGRP.arn}"
@@ -75,33 +75,55 @@ resource "aws_lb_listener" "EAD-CA-LB-LISTENER" {
 #--------------------------------------------------------
 
 resource "aws_autoscaling_group" "EAD-CA-AUTOSCALEGRP" {
-  name                 = "${var.KEY_NAME}-autoscalegroup"
+  name                      = "${var.KEY_NAME}-autoscalegroup"
 
-  launch_configuration = "${aws_launch_configuration.EAD-CA-LAUNCH-CONFIG.id}"
-  availability_zones  = ["${data.aws_availability_zones.all.names}"]
+  launch_configuration      = "${aws_launch_configuration.EAD-CA-LAUNCH-CONFIG.id}"
+  availability_zones        = ["${data.aws_availability_zones.all.names}"]
 
-  min_size            = "${var.NUMBER_OF_INSTANCES}"
-  max_size            = "${var.NUMBER_OF_INSTANCES}"
-  desired_capacity    = "${var.NUMBER_OF_INSTANCES}"
+  vpc_zone_identifier       = ["${data.aws_subnet_ids.all.ids}"]
 
-  health_check_type   = "ELB"
-  health_check_grace_period = "100"
+  min_size                  = "${var.NUMBER_OF_INSTANCES}"
+  max_size                  = "${var.NUMBER_OF_INSTANCES}"
+  desired_capacity          = "${var.NUMBER_OF_INSTANCES}"
 
-  target_group_arns   = ["${aws_lb_target_group.EAD-CA-TARGETGRP.arn}"]
+  health_check_type         = "ELB"
+  health_check_grace_period = "300"
+  default_cooldown          = "60"
+
+  target_group_arns         = ["${aws_lb_target_group.EAD-CA-TARGETGRP.arn}"]
+  
 
   tags = {
-    key               = "Name"
-    value             = "${var.KEY_NAME}-WebServer"
-    propagate_at_launch = true
+    key                     = "Name"
+    value                   = "${var.KEY_NAME}-WebServer"
+    propagate_at_launch     = true
   }
+}
+
+resource "aws_autoscaling_attachment" "EAD-CA-AUTOSCALE-ATTACH" {
+  
+  alb_target_group_arn       = "${aws_lb_target_group.EAD-CA-TARGETGRP.arn}"
+  autoscaling_group_name     = "${aws_autoscaling_group.EAD-CA-AUTOSCALEGRP.id}"
+
 }
 
 resource "aws_launch_configuration" "EAD-CA-LAUNCH-CONFIG" {
   name              = "${var.KEY_NAME}-launchconfigurator"
   image_id          = "${var.AMI}"
   instance_type     = "${var.INSTANCE_TYPE}"
-  security_groups   = ["${aws_security_group.EAD-CA-SECURITYGROUP.id}"]
+  security_groups   = ["${aws_security_group.EAD-CA-SECURITYGROUP-AUTOSCALE.id}"]
   
+  root_block_device {
+    volume_type = "standard"
+    volume_size = "80"
+  }
+
+  ephemeral_block_device {
+    device_name = "/dev/xvdg"
+    virtual_name = "ephemeral0"
+  }
+
+
   lifecycle {
     create_before_destroy = true
   }
@@ -112,9 +134,9 @@ resource "aws_launch_configuration" "EAD-CA-LAUNCH-CONFIG" {
 #
 #
 #--------------------------------------------------------
-resource "aws_security_group" "EAD-CA-SECURITYGROUP" {
-  name                = "${var.KEY_NAME}-securitygrp"
-  description         = "Allow HTTP Traffic for CA"
+resource "aws_security_group" "EAD-CA-SECURITYGROUP-AUTOSCALE" {
+  name                = "${var.KEY_NAME}-securitygrp-autoscalegrp"
+  description         = "Allow HTTP and SSL Traffic to the instances in autoscale group"
   vpc_id              = "${data.aws_vpc.default.id}"
 
   ingress {
@@ -131,6 +153,13 @@ resource "aws_security_group" "EAD-CA-SECURITYGROUP" {
     cidr_blocks       = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Author            = "${var.CA_AUTHOR_NAME}"
     Purpose           = "${var.CA_PURPOSE}"
@@ -141,4 +170,52 @@ resource "aws_security_group" "EAD-CA-SECURITYGROUP" {
     create_before_destroy = true
   }
 
+}
+
+resource "aws_security_group" "EAD-CA-SECURITYGROUP-LOADBALANCER" {
+  name                = "${var.KEY_NAME}-securitygrp-loadbalancer"
+  description         = "Allow HTTP Traffic to the Load Balancer Only"
+  vpc_id              = "${data.aws_vpc.default.id}"
+
+  ingress {
+    from_port         = 80
+    to_port           = 80
+    protocol          = "TCP"
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Author            = "${var.CA_AUTHOR_NAME}"
+    Purpose           = "${var.CA_PURPOSE}"
+    Environment       = "${var.CA_ENVIRONMENT}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+#--------------------------------------------------------
+# CREATE A SNS TOPIC RESOURCES
+#
+#
+#--------------------------------------------------------
+resource "aws_sns_topic" "EAD-CA-SNS-TOPIC-TESTRESULT" {
+  name                = "${var.KEY_NAME}-test-results-notify"
+  display_name        = "${var.KEY_NAME} Test Results Notificiation"
+}
+
+resource "aws_sns_topic_subscription" "EAD-CA-SNS-SUBSCRIPTION-TESTRESULT" {
+  topic_arn               = "${aws_sns_topic.EAD-CA-SNS-TOPIC-TESTRESULT.arn}"
+  protocol                = "sms"
+  endpoint                = "${var.CA_AUTHOR_PHONE}"
+  endpoint_auto_confirms  = true
 }
--- infra/vars.tf
+++ infra/vars.tf
@@ -2,17 +2,30 @@ variable "ACCESS_KEY" {}
 variable "SECRET_KEY" {}
 
 variable "AMI" {
+  description = " This is the image used in the Launch Configuration"
   default = "ami-03d6469eda9235f8e"
 }
 
 variable "NUMBER_OF_INSTANCES" {
   description= "The number of instances that are in the auto scale group"
-  default = 6
+  default = 3
 }
 
 variable "REGION" {
   description = "Default Region for creating instances"
   default = "eu-west-1"
+}
+
+variable "PROTOCOL_HTTP" {
+  default="HTTP"
+}
+
+variable "PORT_HTTP" {
+  default = "80"
+}
+
+variable "PORT_SSL" {
+  default = "22"
 }
 
 variable "INSTANCE_TYPE" {
@@ -25,6 +38,20 @@ variable "KEY_NAME" {
 
 variable "CA_AUTHOR_NAME" {
   default = "Declan Smyth"
+}
+
+variable "CA_AUTHOR_EMAIL" {
+  default = "declan.smyth@gmail.com"
+}
+
+
+variable "CA_AUTHOR_PHONE" {
+  default = ""
+}
+
+variable "SUBSCRIBER_EMAIL" {
+  description = "This is the email address for subscription to the topic operator"
+  default = ""
 }
 
 variable "CA_ENVIRONMENT" {

243f8630871b26124e69233b153d5233dbf42eef
Declan Smyth <declans@avaya.com>
2019-04-10 23:58:47 +0100

    Added support for Target Groups - Implemented changes to check target groups list of instances - Add functions to get IDs and Instances from Target Groups - Updated all test checks to monitor the target group for replacements instances -Updated AWS configuration with additional topic ARNs - Removed test code and debug statements - Improved test results information to display better information - refactored code to improve environment reinstatement checks

--- scripts/awsinformation.py
+++ scripts/awsinformation.py
@@ -9,6 +9,7 @@
 # ######################################
 
 import boto3
+import botocore
 import json
 
 # Setup an EC2 Client
@@ -19,6 +20,9 @@ autoScaleClient = boto3.client('autoscaling')
 
 # Setup an EC2 Resource
 ec2Resource = boto3.resource('ec2')
+
+# Setup an ELB Client
+elbv2Client = boto3.client('elbv2')
 
 ###############################################################################
 # Function Definitions
@@ -45,7 +49,8 @@ def GetListOfRunningInstancesById(instanceIds):
         ec2Instances = ec2Resource.instances.filter(
                         Filters=[{'Name': 'instance-state-name', 'Values': ['running']}], InstanceIds=instanceIds)
         instanceList = [instance for instance in ec2Instances]
-        return instanceList
+        instanceIdList = [instance.id for instance in ec2Instances]
+        return instanceList, instanceIdList
 
 
 ###############################################################################
@@ -110,15 +115,26 @@ def GetAutoScaleGroupInstancesIDs(groupname):
     return  instanceIDList
 
 ###############################################################################
+# -- Function: Get List of instances IDs from the autoscale group
+#               Input: Autoscale Group Name
+#               Return: List of Instance IDs in the Auto Scaling Group
+def GetInstancesIDFromTargetInstances(instances):
+        instanceIDList=[]
+        for i in instances:
+                instanceIDList.append(i['Target']['Id'])
+        return instanceIDList
+
+
+
+###############################################################################
 # -- Function: Get List of instances in an autoscale group
 #               Input: Autoscale Group Name
 #               Return: Desired Size of Group
-def GetAutoScaleGroupSize(groupname):
+def GetAutoScaleInformation(groupname):
         autoScaleInfo = autoScaleClient.describe_auto_scaling_groups(AutoScalingGroupNames=[groupname])
         for i in autoScaleInfo['AutoScalingGroups']:
-            groupsize = dict(desired=i['DesiredCapacity'], max=i['MaxSize'],min=i['MinSize'])
-        return groupsize
-
+            group_size_info= dict(desired=i['DesiredCapacity'], max=i['MaxSize'],min=i['MinSize'])
+        return group_size_info 
 
 ###############################################################################
 # -- Function: Get Autoscale Target Group
@@ -131,3 +147,20 @@ def GetAutoScaleGroupTargetGrp(groupname):
         return TargetGroupARN
 
 
+###############################################################################
+# -- Function: Get Target Group Health Information
+#               Input:  Target Group ARN
+#               Return: ARN of Target Group
+def GetTargetGrpInstances(TargetGroupARN):
+        targetGrpHealthInfo = elbv2Client.describe_target_health(TargetGroupArn=TargetGroupARN)
+        
+        target_group_instances = targetGrpHealthInfo['TargetHealthDescriptions']  
+        
+        target_group_instances_ids = []
+
+        for instance in targetGrpHealthInfo['TargetHealthDescriptions']:
+                target_group_instances_ids.append(instance['Target']['Id'])
+
+        return target_group_instances_ids, target_group_instances
+
+ 
--- scripts/testharrness.py
+++ scripts/testharrness.py
@@ -23,7 +23,8 @@ import awsinformation
 ###############################################################################
 awsConfiguration = {
         "autoscalegroup_name": "ead-ca-autoscalegroup",
-        "topic_arn": "arn:aws:sns:eu-west-1:197110341471:ead-ca-test-results-notify"
+        "topic_arn_sms": "arn:aws:sns:eu-west-1:197110341471:ead-ca-test-results-notify", # Automatically setup in the environment
+        "topic_arn_email": "arn:aws:sns:eu-west-1:197110341471:AWS-Test-Results" # Manually created in the environment
 }
 
 
@@ -70,9 +71,6 @@ def RandomlySelectEC2Instances(instLst, numSelected):
 #               Input: Number of Instances to Terminate
 #               Return: List of Instance IDs to Terminatated
 def TerminateInstances(ec2Res,terminateLst):
-        print ("The following instances are now being TERMINATED")
-        for instance in terminateLst:
-                print (instance)
         ec2Res.instances.filter(InstanceIds=terminateLst).terminate()
 
 
@@ -85,8 +83,8 @@ def TerminateInstances(ec2Res,terminateLst):
 #               Return: List of Instance IDs to Terminatated
 def CalculateTimeOutValue():
         # Setup Test Timeout value in Seconds
-        testTimeoutValue = 150 * iNumberInstancesToDisrupt
-        print ("\n *** TEST TIMEOUT SET TO %s seconds ***" % testTimeoutValue)
+        testTimeoutValue = 220 * iNumberInstancesToDisrupt
+        #print ("\n *** TEST TIMEOUT SET TO %s seconds ***" % testTimeoutValue)
         return testTimeoutValue 
 
 
@@ -120,27 +118,35 @@ ec2Client = boto3.client('ec2')
 ec2Resource = boto3.resource('ec2')
 
 # Get List of instances in the Auto Scale Group
-instanceListAutoScaleGrp = awsinformation.GetAutoScaleGroupInstances(awsConfiguration["autoscalegroup_name"])
+#instanceListAutoScaleGrp = awsinformation.GetAutoScaleGroupInstances(awsConfiguration["autoscalegroup_name"])
 
 # Get List of instance IDs in the Auto Scale Group
-instanceIDListAutoScaleGrp = awsinformation.GetAutoScaleGroupInstancesIDs(awsConfiguration["autoscalegroup_name"])
+#instanceIDListAutoScaleGrp = awsinformation.GetAutoScaleGroupInstancesIDs(awsConfiguration["autoscalegroup_name"])
+
+# Get the Target Group ARN
+targetGroupARN = awsinformation.GetAutoScaleGroupTargetGrp(awsConfiguration["autoscalegroup_name"])
+
+# Get Target Group Instance IDs
+target_group_instances_ids, target_group_instances = awsinformation.GetTargetGrpInstances(targetGroupARN)
 
 # Get Auto Scale Group Size information
-autoScaleGroupSize = awsinformation.GetAutoScaleGroupSize(awsConfiguration["autoscalegroup_name"])
+autoScaleGroupInfo = awsinformation.GetAutoScaleInformation(awsConfiguration["autoscalegroup_name"])
 
-# Get List of Running Instances based on Conent of Auto Scale Group
-instanceFullList = awsinformation.GetListInstancesById(instanceIDListAutoScaleGrp)
+# Get List of all Instances based on Content of Target Group
+instanceFullList = awsinformation.GetListInstancesById(target_group_instances_ids)
 
-# Get List of Running Instances based on Conent of Auto Scale Group
-instanceRunList = awsinformation.GetListOfRunningInstancesById(instanceIDListAutoScaleGrp)
+# Get List of Running Instances based on Content of Target Group
+instanceRunList, instanceIdRunList = awsinformation.GetListOfRunningInstancesById(target_group_instances_ids)
 
 # Get Number of Instances Running
 numInstancesRunning = len(instanceRunList)
 
 # Print the Instance Information on Screen
 if numInstancesRunning > 0 :
-       printinfo.PrintSAutoScaleGroupInfo(instanceListAutoScaleGrp)
+       #printinfo.PrintSAutoScaleGroupInfo(instanceListAutoScaleGrp)
        printinfo.PrintInformationToScreen(instanceFullList)
+       if numInstancesRunning < autoScaleGroupInfo['desired']:
+               print ("NOTE: The number of instances running does not match the desired level of your auto scale group")
 else:
         print ("""\
 There are ZERO instances running in your environment
@@ -154,14 +160,14 @@ if numInstancesRunning > 0:
 # If the user has selected a number to disrupt and there are instnaces in the Auto Scale Group
 # Start  to run the ddisruption tests
 if iNumberInstancesToDisrupt > 0 and numInstancesRunning > 0:
-        # Select Instances to Terminate
+        # Select Instances to Terminate from the list of running targets
         disruptList = RandomlySelectEC2Instances(instanceRunList,iNumberInstancesToDisrupt)
         
         # Get the number of instanace IDs selected to be terminated
         numInstancesToTerminate = len(disruptList)
 
         # Print Test Start Message
-        printinfo.PrintTestStart(numInstancesToTerminate)
+        printinfo.PrintTestStart(numInstancesToTerminate, disruptList)
 
         # Start Time to determine how long it takes to recover the system
         testStartTime = datetime.datetime.now()
@@ -173,80 +179,102 @@ if iNumberInstancesToDisrupt > 0 and numInstancesRunning > 0:
         # Terminate Instances
         TerminateInstances(ec2Resource,disruptList)
 
-        # Get List of Running Instances
-        instanceRunList = awsinformation.GetListOfRunningInstances()
-        testStartNumRunning = runningListNum = len(instanceRunList)
+        # Get list of running instances after the selected instances have been terminated 
+        instanceRunListAfterTerminiation, instanceIdRunListAfterTermination = awsinformation.GetListOfRunningInstancesById(target_group_instances_ids)
 
-        # Print Instance Information to Screen
-        printinfo.PrintInformationToScreen(instanceRunList)
+        # Keep the number of running instances at the start of the test
+        testStartNumRunning = runningListNum = len(instanceRunListAfterTerminiation)
 
-        
-        while runningListNum != numInstancesRunning and timedout == False:
+        # Print list of running instances after the termination
+        printinfo.PrintInformationToScreen(instanceRunListAfterTerminiation)
+        print ("\nPlease wait for your system to be recovered... \n")
+        printinfo.PrintThinLine
+
+        # Desired State is:
+        #       - a. Number of Instances in the target group equals desired number in auto scaling group
+        #       - b. All instances in the target group are healthy
+        desired_state = False
+        count = 0
+
+
+        while desired_state == False and timedout == False:
+                count+=1
+
+                # Get the list of Instances in the target group at the start of the test cycle
+                target_group_instances_ids_in_test, target_group_instances_in_test = awsinformation.GetTargetGrpInstances(targetGroupARN)      
                 
-                # Get the list of instances in a starting state
-                instanceIDListAutoScaleGrp = awsinformation.GetAutoScaleGroupInstancesIDs(awsConfiguration["autoscalegroup_name"])
-                startingList = awsinformation.GetListOfPendingInstancesById(instanceIDListAutoScaleGrp)
+                # Test 1 - Check if the number of instances in the group is the equal to desired level of the auto scale group
+                if len(target_group_instances_ids_in_test) == autoScaleGroupInfo['desired']:
+                        test_1_number_of_instances_in_group = True
+                else:
+                        test_1_number_of_instances_in_group = False
+                
+                # Test 2 - Check if all instances in the group are healthy
+                for target in target_group_instances_in_test:
+                        #print (target['TargetHealth']['State'])
+                        #print ("------")
+                        if target['TargetHealth']['State'] == 'healthy':
+                                test_2_instances_are_healthy = True
+                        else:
+                                test_2_instances_are_healthy = False
 
-                # Get the list of instances in a running state
-                #instanceIDListAutoScaleGrp = awsinformation.GetAutoScaleGroupInstancesIDs(awsConfiguration["autoscalegroup_name"])
-                instanceRunList = awsinformation.GetListOfRunningInstancesById(instanceIDListAutoScaleGrp)
-
-                # Get the number of instances in a list
-                runningListNum = len(instanceRunList)
-                startingListNum = len(startingList)
+                # Check the Status of the tests...
+                if test_1_number_of_instances_in_group and test_2_instances_are_healthy:
+                        desired_state = True
+                else:
+                        desired_state = False
 
                 # Add a time out to ensure the testing does not run forever
                 if datetime.datetime.now() > testStartTime + datetime.timedelta(seconds=testTimeoutValue):
                         timedout = True
-                        print ("Timed Out !!!!")
-                else:
-                        printinfo.PrintThinLine()
-                        tmp=awsinformation.GetAutoScaleGroupInstances(awsConfiguration["autoscalegroup_name"])
-                        print (tmp)
-                        printinfo.PrintThinLine()
-                        time.sleep(60)
+                        #print ("Timed Out !!!!")
+
+                if (desired_state == False and timedout == False):
+                        time.sleep(15) # Sleep for 15 Seconds
         else:
                 # Perform Calculations when the tests stop
                 testStopTime = datetime.datetime.now()
 
+                instanceRunListAfterTest, instanceIdRunListAfterTest = awsinformation.GetListOfRunningInstancesById(target_group_instances_ids_in_test)
+               
+                # Get the list of new instance IDs that have been added to the target group
+                newInstanceIDs = list(set(instanceIdRunListAfterTermination) ^ set(instanceIdRunListAfterTest))
+
                 # Get the number of instances that were created
-                instancesRestarted = runningListNum - testStartNumRunning
+                instancesRestarted = len(newInstanceIDs)
 
                 # Calculate the elapsed time for the test
                 elapsedTime = testStopTime - testStartTime
 
                 # Check if the test timed out or completed within the expected time
-        
                 if not timedout:
                         testStatus="PASSED"
-                        notifyMessage = {
-                                "starttime" : '{0:%Y-%m-%d %H:%M:%S}'.format(testStartTime),
-                                "endtime":'{0:%Y-%m-%d %H:%M:%S}'.format(testStopTime),
-                                "instancesrestarted":instancesRestarted,
-                                "elapsedtime": str(elapsedTime),
-                                "teststatus": testStatus
-                        }
-                        print ("The test has now  completed. There are %s instances running" % runningListNum)
+                        print ("The test has now  completed. There are %s instances running" % len(instanceIdRunListAfterTest))
                 else:
                         testStatus="FAILED:TIMED-OUT"
-                        notifyMessage = {
-                                "starttime" : '{0:%Y-%m-%d %H:%M:%S}'.format(testStartTime),
-                                "endtime":'{0:%Y-%m-%d %H:%M:%S}'.format(testStopTime),
-                                "instancesrestarted":instancesRestarted,
-                                "elapsedtime":str(elapsedTime),
-                                "teststatus":testStatus
-                        }
                         print ("The test has TIMEDOUT. Your instances did not restart within the time period allowed.")
+
+                # Construct the Notification Messatge
+                notifyMessage = {
+                        "starttime" : '{0:%Y-%m-%d %H:%M:%S}'.format(testStartTime),
+                        "endtime":'{0:%Y-%m-%d %H:%M:%S}'.format(testStopTime),
+                        "instancesstopped":numInstancesToTerminate,
+                        "instancesrestarted":instancesRestarted,
+                        "elapsedtime": str(elapsedTime),
+                        "teststatus": testStatus,
+                        "newinstanceid": newInstanceIDs
+                }
 
                 # Print Infomration on screen
                 printinfo.PrintTestResult(notifyMessage)
                 
                 # Print Instance Information to Screen
-                printinfo.PrintInformationToScreen(instanceRunList)
+                printinfo.PrintInformationToScreen(instanceRunListAfterTest)
                 
                 # Send Notifications of test results
-                notification.SendEmailNotification(notifyMessage)
-                notification.SendSMSNotification(notifyMessage)
+                notification.SendNotification(notifyMessage)
+                #notification.SendEmailNotification(notifyMessage, awsConfiguration['topic_arn_email'])
+                notification.SendSMSNotification(notifyMessage, awsConfiguration['topic_arn_sms'])
 # Print a line at the end... 
 printinfo.PrintThickLine()
 
